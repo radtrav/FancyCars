8fd92fd6c554501f2784455d364749fe1835ff25
Travis Young <t.w.young.ubc@gmail.com>
2018-04-02 12:55:18 -0400

    removed unused action types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,8 +1,4 @@
 export const FETCH_CARS_REQUEST = 'fetch_cars_request';
 export const FETCH_CARS_SUCCESS = 'fetch_cars_success';
 export const FETCH_CARS_FAILURE = 'fetch_cars_failure';
-export const SORT_BY_NAME = 'sort_by_name';
-export const SORT_BY_MAKE = 'sort_by_make';
-export const SORT_BY_YEAR = 'sort_by_year';
-export const SORT_BY_AVAILABILITY = 'sort_by_availability';
 export const SORT_BY = 'sort_by';
